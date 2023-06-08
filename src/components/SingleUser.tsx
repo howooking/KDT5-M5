18fa2468754f4fc99636c33fb951c50cb3e24259
Tteum00 <wjdtmddnjs0913@naver.com>
2023-06-08 13:29:00 +0900

    충동 해결

--- src/components/SingleUser.tsx
+++ src/components/SingleUser.tsx
@@ -1,4 +1,5 @@
 import styles from '../routes/Admin/UserCheck.module.css';
+import defaultImage from '../assets/defaultProfile.jpg';
 
 interface SingleUserProps {
   data: CheckUser;
@@ -7,12 +8,19 @@ interface SingleUserProps {
 
 export default function SingleUser({ data, index }: SingleUserProps) {
   const even = index % 2 === 0 ? styles.even : '';
-  
+
+  const defaultImageSrc = defaultImage;
+
+  const profileImage = data.profileImg || defaultImageSrc;
+
   return (
     <tr className={even}>
-      <td>{index}</td>
-      <td>{data.email}</td>
-      <td>{data.displayName}</td>
+      <td className={styles.number}>{index}</td>
+      <td className={styles.email}>{data.email}</td>
+      <td className={styles.Name}>{data.displayName}</td>
+      <td>
+        <img src={profileImage} alt="Profile" className={styles.profileImg} />
+      </td>
     </tr>
   );
 }
